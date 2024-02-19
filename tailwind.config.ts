abb4e2f2972b6fa8d0c83d3a5a6558d5db398c89
timwitzdam <tim2013w@gmail.com>
2024-02-19 20:35:42 +0100

    Applied default theme + styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        bg: "#FFF6E9",
+        primary: "#4CB9E7",
+        secondary: "#2C2C2C",
       },
     },
   },
